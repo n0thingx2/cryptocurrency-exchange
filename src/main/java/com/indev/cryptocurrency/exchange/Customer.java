4c61e6421a8baa2edba8a0edae674a3e3eda8cbd
Mohamed Boufous <boufousmohamed@outlook.com>
2019-02-11 11:10:01 +0100

    Implementing customer Bitcoin wallet creation.

--- src/main/java/com/indev/cryptocurrency/exchange/Customer.java
+++ src/main/java/com/indev/cryptocurrency/exchange/Customer.java
@@ -1,8 +1,23 @@
 package com.indev.cryptocurrency.exchange;
 
 public class Customer {
-    public Customer withCryptocurrency(String bitcoin, int i) {
-        return null;
+
+    String cryptocurrencyName;
+    int cryptocurrencyBalance;
+
+    public Customer withCryptocurrency(String cryptocurrencyName, int cryptocurrencyBalance) {
+        this.cryptocurrencyName = cryptocurrencyName;
+        this.cryptocurrencyBalance = cryptocurrencyBalance;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "0:$" +
+                "," +
+                cryptocurrencyBalance +
+                ":" +
+                cryptocurrencyName;
     }
 
     public Customer withBalance(int i) {
